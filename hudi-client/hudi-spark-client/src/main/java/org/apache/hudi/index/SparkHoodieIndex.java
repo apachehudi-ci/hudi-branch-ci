uber/hudi

[HUDI-2157] Add the bucket index and its read/write implemention of Spark engine.

hudi-client/hudi-spark-client/src/main/java/org/apache/hudi/index/SparkHoodieIndex.java
@@ -31,6 +31,7 @@ import org.apache.hudi.config.HoodieWriteConfig;
 import org.apache.hudi.exception.HoodieIndexException;
 import org.apache.hudi.index.bloom.SparkHoodieBloomIndex;
 import org.apache.hudi.index.bloom.SparkHoodieGlobalBloomIndex;
+import org.apache.hudi.index.bucket.SparkHiveBucketIndex;
 import org.apache.hudi.index.hbase.SparkHoodieHBaseIndex;
 import org.apache.hudi.index.simple.SparkHoodieGlobalSimpleIndex;
 import org.apache.hudi.index.simple.SparkHoodieSimpleIndex;
@@ -66,6 +67,8 @@ public abstract class SparkHoodieIndex<T extends HoodieRecordPayload> extends Ho
         return new SparkHoodieSimpleIndex(config);
       case GLOBAL_SIMPLE:
         return new SparkHoodieGlobalSimpleIndex(config);
+      case BUCKET_INDEX:
+        return new SparkHiveBucketIndex(config);
       default:
         throw new HoodieIndexException("Index type unspecified, set " + config.getIndexType());
     }

hudi-spark-datasource/hudi-spark-common/src/main/scala/org/apache/hudi/DataSourceOptions.scala
@@ -479,6 +479,12 @@ object DataSourceWriteOptions {
     .defaultValue("io.confluent.kafka.serializers.KafkaAvroDeserializer")
     .sinceVersion("0.9.0")
     .withDocumentation("This class is used by kafka client to deserialize the records")
+
+  // bucketSpec: CLUSTERED BY (trace_id) SORTED BY (trace_id ASC) INTO 65536 BUCKETS
+  val HIVE_BUCKET_SPEC_OPT: ConfigProperty[String] = ConfigProperty
+    .key("hoodie.datasource.hive_sync.bucket_spec")
+    .noDefaultValue()
+    .withDocumentation("Hive metastore bucket specification.")
 }
 
 object DataSourceOptionsHelper {

apachehudi-ci/hudi-branch-ci

hudi-client/hudi-spark-client/src/main/java/org/apache/hudi/index/SparkHoodieIndex.java
@@ -31,6 +31,7 @@ import org.apache.hudi.config.HoodieWriteConfig;
 import org.apache.hudi.exception.HoodieIndexException;
 import org.apache.hudi.index.bloom.SparkHoodieBloomIndex;
 import org.apache.hudi.index.bloom.SparkHoodieGlobalBloomIndex;
+import org.apache.hudi.index.bucket.SparkBucketIndex;
 import org.apache.hudi.index.hbase.SparkHoodieHBaseIndex;
 import org.apache.hudi.index.simple.SparkHoodieGlobalSimpleIndex;
 import org.apache.hudi.index.simple.SparkHoodieSimpleIndex;
@@ -66,6 +67,8 @@ public abstract class SparkHoodieIndex<T extends HoodieRecordPayload> extends Ho
         return new SparkHoodieSimpleIndex(config);
       case GLOBAL_SIMPLE:
         return new SparkHoodieGlobalSimpleIndex(config);
+      case BUCKET_INDEX:
+        return new SparkBucketIndex(config);
       default:
         throw new HoodieIndexException("Index type unspecified, set " + config.getIndexType());
     }

hudi-spark-datasource/hudi-spark-common/src/main/scala/org/apache/hudi/DataSourceOptions.scala
@@ -463,6 +463,12 @@ object DataSourceWriteOptions {
     .noDefaultValue()
     .withDocumentation("Mode to choose for Hive ops. Valid values are hms, jdbc and hiveql.")
 
+  // bucketSpec: CLUSTERED BY (trace_id) SORTED BY (trace_id ASC) INTO 65536 BUCKETS
+  val HIVE_SYNC_BUCKET_SPEC: ConfigProperty[String] = ConfigProperty
+    .key("hoodie.datasource.hive_sync.bucket_spec")
+    .noDefaultValue()
+    .withDocumentation("Hive metastore bucket specification.")
+
   // Async Compaction - Enabled by default for MOR
   val ASYNC_COMPACT_ENABLE: ConfigProperty[String] = ConfigProperty
     .key("hoodie.datasource.compaction.async.enable")