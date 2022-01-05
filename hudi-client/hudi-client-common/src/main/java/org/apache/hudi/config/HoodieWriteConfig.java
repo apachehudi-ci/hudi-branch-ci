b9a9119bf3894a52a100a1cdd04c5bec44f2ed73
Manoj Govindassamy <manoj.govindassamy@gmail.com>
2022-01-05 13:32:54 -0800

    [HUDI-1295] Metadata Index - Bloom filter and Column stats index to speed up index lookups
     - HoodieBloomMetaIndexBatchCheckFunction cleanup to remove unused classes
    
     - Base file checking before reading the file footer for bloom or column stats

--- hudi-client/hudi-client-common/src/main/java/org/apache/hudi/config/HoodieWriteConfig.java
+++ hudi-client/hudi-client-common/src/main/java/org/apache/hudi/config/HoodieWriteConfig.java
@@ -1437,8 +1437,7 @@ public class HoodieWriteConfig extends HoodieConfig {
   }
 
   public boolean isMetaIndexColumnStatsForAllColumns() {
-    return isMetadataTableEnabled() && isMetaIndexColumnStatsForAllColumns()
-        && getMetadataConfig().isMetaIndexColumnStatsForAllColumns();
+    return isMetadataTableEnabled() && getMetadataConfig().isMetaIndexColumnStatsForAllColumns();
   }
 
   public boolean isMetaIndexBloomFilterBatchLoadEnabled() {
