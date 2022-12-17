d4821274f89bb426539c77b2f56afb002a46bf05
Alexey Kudinkin <alexey@infinilake.com>
2022-12-16 18:21:37 -0800

    Fixing compilation (after rebase)

--- hudi-common/src/main/java/org/apache/hudi/BaseHoodieTableFileIndex.java
+++ hudi-common/src/main/java/org/apache/hudi/BaseHoodieTableFileIndex.java
@@ -50,6 +50,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -337,7 +338,7 @@ public abstract class BaseHoodieTableFileIndex implements AutoCloseable {
             .collect(Collectors.toMap(Pair::getKey, p -> p.getRight().get()));
 
     Set<Path> missingPartitionPaths =
-        CollectionUtils.diff(partitionPaths, cachedPartitionPaths.keySet());
+        CollectionUtils.diffSet(new HashSet<>(partitionPaths), cachedPartitionPaths.keySet());
 
     // NOTE: We're constructing a mapping of absolute form of the partition-path into
     //       its relative one, such that we don't need to reconstruct these again later on
