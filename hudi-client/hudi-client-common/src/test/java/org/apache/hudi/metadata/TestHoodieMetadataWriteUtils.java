abdf2b4acbeaa1e36d368cecf52f80b944ae17b1
Shuo Cheng <njucshuo@gmail.com>
2026-08-24 22:55:13 +0800

    feat(metadata): add standalone metadata table services tool

--- hudi-client/hudi-client-common/src/test/java/org/apache/hudi/metadata/TestHoodieMetadataWriteUtils.java
+++ hudi-client/hudi-client-common/src/test/java/org/apache/hudi/metadata/TestHoodieMetadataWriteUtils.java
@@ -129,6 +129,25 @@ public class TestHoodieMetadataWriteUtils {
     assertEquals(hfileBloomFilterEnabled, metadataWriteConfig.hfileBloomFilterEnabled());
   }
 
+  @Test
+  void testCreateMetadataWriteConfigPropagatesTableServiceSchedulingDelegation() {
+    HoodieMetadataConfig metadataConfig = HoodieMetadataConfig.newBuilder()
+        .withTableServiceManagerEnabled(true)
+        .withTableServiceManagerScheduleActions(ActionType.compaction.name())
+        .build();
+    HoodieWriteConfig writeConfig = HoodieWriteConfig.newBuilder()
+        .withPath("/tmp/base_path/")
+        .withMetadataConfig(metadataConfig)
+        .build();
+
+    HoodieWriteConfig metadataWriteConfig = HoodieMetadataWriteUtils.createMetadataWriteConfig(
+        writeConfig, HoodieFailedWritesCleaningPolicy.EAGER, HoodieTableVersion.EIGHT);
+
+    assertTrue(metadataWriteConfig.getTableServiceManagerConfig().isTableServiceManagerEnabled());
+    assertEquals(ActionType.compaction.name(),
+        metadataWriteConfig.getMetadataConfig().getTableServiceManagerScheduleActions());
+  }
+
   @ParameterizedTest
   @EnumSource(value = MetricsReporterType.class, names = {
       "GRAPHITE", "JMX", "PROMETHEUS_PUSHGATEWAY", "M3", "PROMETHEUS"
